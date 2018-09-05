874f47f241b9fedabbaa55ab6f8ed01bb30f376a
Khris Kempis <khriskempis@gmail.com>
2018-09-05 12:36:03 -0700

    updated item/router.js

--- items/router.js
+++ items/router.js
@@ -9,6 +9,24 @@ const router = express.Router();
 
 const jsonParser = bodyParser.json(); 
 
-// router.post('/', jsonParser, (req,res) => {
-// 	const requiredFields = 
-// });
+// CREATE a new item 
+router.post('/', jsonParser, (req,res) => {
+	const requiredFields = ["name", "description", "qty", "cost", "price", "image_url"];
+
+	Item.create({
+		name: req.body.name,
+		description: req.body.description,
+		qty: req.body.qty,
+		cost: req.body.cost,
+		price: {
+			regular: req.body.price.regular,
+			sale: req.body.price.sale
+		},
+		image_url: {url: req.body.image_url.url}
+	})
+	.then(item => res.status(201).json(item.serialize()))
+	.catch(err => {
+		console.error(err);
+		res.status(500).json({message: 'Internal Server Error'});
+	});
+});
