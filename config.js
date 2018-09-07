f6af2e554cc56e2ac7184c2087753e86f2a1c430
Khris Kempis <khriskempis@gmail.com>
2018-09-07 11:44:37 -0700

    updated config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/eCommerce';
-exports.TEST_DATABAS_URL = process.env.TEST_DATABAS_URL || 'mongodb://localhost/test-eCommerce';
+exports.TEST_DATABAS_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-eCommerce';
 exports.PORT = process.env.PORT || 8080; 
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRT = process.env.JWT_EXPIRY || '7d'; 
