2497ea2dec411e70894d8a54624125df67498c8a
Khris Kempis <khriskempis@gmail.com>
2018-09-07 11:41:40 -0700

    added closeServer to test

--- test/test-eCommerce.js
+++ test/test-eCommerce.js
@@ -1,7 +1,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const { app, runServer, closeServer } = require('../server'); 
+const { app, runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');  
 
 
 const expect = chai.expect;
