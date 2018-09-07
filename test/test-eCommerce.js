ee67b344fd7a8f5c385e8c0f20277e7a8f3c44be
Khris Kempis <khriskempis@gmail.com>
2018-09-07 11:35:53 -0700

    added closeServer to test

--- test/test-eCommerce.js
+++ test/test-eCommerce.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const { app } = require('../server'); 
+const { app, runServer closeServer } = require('../server'); 
 
 
 const expect = chai.expect;
@@ -9,6 +9,14 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Server working', function() {
+
+	before(function() {
+		return runServer(TEST_DATABASE_URL);
+	});
+
+	after(function() {
+		return closeServer(); 
+	});
 
 	it('should serve static assets to client', function() {
 		return chai.request(app)
